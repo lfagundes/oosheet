ada639ffba7bf1320412af8b00d38df6f6823ed2
lfagundes <lfagundes@jarjar.(none)>
2010-12-16 10:01:37 -0200

    setup.py finds packages to install

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import os
 
-setup(name='oosheet',
-      version='0.1',
-      description='OpenOffice.org Spreadsheet scripting library',
+setup(name = 'oosheet',
+      version = '0.1',
+      description = 'OpenOffice.org Spreadsheet scripting library',
       long_description = open(os.path.join(os.path.dirname(__file__), "README")).read(),
       author = "Luis Fagundes",
       author_email = "lhfagundes@hacklab.com.br",
-      license="The MIT License",
-      url='http://oosheet.hacklab.com.br/',
+      license = "The MIT License",
+      packages = find_packages(),
+      install_requires = ['uno'],
+#      url = 'http://oosheet.hacklab.com.br/',
       classifiers = [
           'Intended Audience :: Developers',
           'Natural Language :: English',
